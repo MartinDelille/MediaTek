cb7d00f90a589fc9fa90fecc926c32b3b494af92
Martin Delille <martin@phonations.com>
2015-05-24 20:11:34 +0200

    display list of media

--- mediatek.js
+++ mediatek.js
@@ -1,23 +1,10 @@
+Medias = new Mongo.Collection("medias");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    medias: function () {
+      return Medias.find({});
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
