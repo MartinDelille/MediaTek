097802c40413f482477742b1f4768fe2008bf904
Martin Delille <martin@phonations.com>
2015-05-24 21:53:58 +0200

    add form to add media

--- mediatek.js
+++ mediatek.js
@@ -6,5 +6,23 @@ if (Meteor.isClient) {
       return Medias.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-media": function (event) {
+      // This function is called when the new media form is submitted
+      var text = event.target.text.value;
+
+      Medias.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });;
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
 }
 
